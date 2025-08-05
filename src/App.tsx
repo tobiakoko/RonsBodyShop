60580336dace85a4af27471e80e9f684bec7f534
Daniel Akoko <42564038+tobiakoko@users.noreply.github.com>
2025-08-05 08:01:39 +0000

    update main app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ContactSection from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
+import NavigationLink from './components/Header/Navigation';
+import HeroSection from './components/Hero/Hero';
+import ServicesSection from './components/Services/Services';
+import About from './components/About/About';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const handleContactSubmit = async (formData: any) => {
+    // send the data to your backend here
+    console.log('Contact form submitted:', formData);
+    
+    // Simulate API call
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    
+    // Show a success message here
+    alert('Thank you! Your message has been sent successfully.');
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="min-h-screen bg-white">
+      <NavigationLink />
+      <main className="">
+          <HeroSection />
+          <About />
+          <ServicesSection />
+          <ContactSection onSubmit={handleContactSubmit} />
+          <Footer />
+      </main>
+    </div>
   )
 }
 
